0e11b97edddeae0c61364b42993579398f4a2ffa
James Dansie <jdansie8336@gmail.com>
2019-07-22 16:18:12 -0700

    added event listener

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 var imageOneEl = document.getElementById('product1');
 var imageTwoEl = document.getElementById('product2');
 var imageThreeEl = document.getElementById('product3');
+var productContainerEl = document.getElementById('product-container');
 var randomArray = [];
 var randomIndex = 0;
 
@@ -55,21 +56,26 @@ function makeRandomArray(){
 
 
 function render(){
-
+  //The index will increment 3 at a time 1,2,3 then 4,5,6
+  //it will be an index for a random unique number from the randomArray
+  //This will return 3 random unique products for iteration
   imageOneEl.src = allProducts[randomArray[randomIndex]].filepath;
   imageOneEl.alt = allProducts[randomArray[randomIndex]].name;
   imageOneEl.title = allProducts[randomArray[randomIndex]].name;
+  allProducts[randomArray[randomIndex]].views++;
 
   imageTwoEl.src = allProducts[randomArray[randomIndex+1]].filepath;
   imageTwoEl.alt = allProducts[randomArray[randomIndex+1]].name;
   imageTwoEl.title = allProducts[randomArray[randomIndex+1]].name;
+  allProducts[randomArray[randomIndex+1]].views++;
 
   imageThreeEl.src = allProducts[randomArray[randomIndex+2]].filepath;
   imageThreeEl.alt = allProducts[randomArray[randomIndex+2]].name;
   imageThreeEl.title = allProducts[randomArray[randomIndex+2]].name;
+  allProducts[randomArray[randomIndex+2]].views++;
   console.log('index is ',randomIndex);
 
-  //each time we load 3 images to increment by 3
+  //each time we load 3 images so increment by 3
   randomIndex += 3;
 
   //if the new index is larger than the array then reset stuff
@@ -78,6 +84,20 @@ function render(){
     makeRandomArray();
   }
 }
+
+function handleClick(){
+  var chosenImg = event.target.title;
+
+  for(var i = 0; i < allProducts.length; i++){
+    if(allProducts[i].name === chosenImg){
+      allProducts[i].views++;
+    }
+  }
+
+  render();
+}
+
+productContainerEl.addEventListener('click', handleClick);
 
 makeRandomArray();
 render();
