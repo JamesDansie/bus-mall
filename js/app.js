c3ee2c4295f7109b6138bfc6ccbdd73554192fdc
James Dansie <jdansie8336@gmail.com>
2019-07-22 19:04:24 -0700

    stuff is done for monday

--- js/app.js
+++ js/app.js
@@ -4,15 +4,19 @@ var imageOneEl = document.getElementById('product1');
 var imageTwoEl = document.getElementById('product2');
 var imageThreeEl = document.getElementById('product3');
 var productContainerEl = document.getElementById('product-container');
+var tableEl = document.getElementById('results');
 var randomArray = [];
 var randomIndex = 0;
 var votes = 0;
 
 
-//Nevermind I don't know how to make require work >_<
-// var imgFolder = './img/';
-// var fs = require('fs');
-// console.log(fs.readdirSync(imgFolder));
+// // Nevermind I don't know how to make require work >_<
+// const testFolder = './img/';
+// const fs = require('fs');
+
+// fs.readdirSync(testFolder).forEach(file => {
+//   console.log(file);
+// });
 
 //Eventually replace this with the zombie code above to make a list that will update with new products
 var products = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
@@ -54,9 +58,55 @@ function makeRandomArray(){
   }
 }
 
+//Makes the Header
+function makeHeader(){
+  //declaring elements
+  var trEl = document.createElement('tr');
+  tableEl.appendChild(trEl);
+
+  //Writing the first element with 'location'
+  addElement('th', 'Product', trEl);
+
+  //Writing the views
+  addElement('th', 'Views', trEl);
+
+  //Writing the clicks
+  addElement('th', 'Clicks', trEl);
+
+  //Writing the clicks per view
+  addElement('th', 'Clicks/View', trEl);
+}
+
+//Make Table Body
+function makeBody(){
+
+  for(var i = 0; i < allProducts.length; i++){
+    //declaring elements
+    var trEl = document.createElement('tr');
+    tableEl.appendChild(trEl);
+
+    addElement('th', allProducts[i].name, trEl);
+    addElement('td', allProducts[i].views, trEl);
+    addElement('td', allProducts[i].votes, trEl);
+
+    //calculating votes per view if NaN then set to zero
+    if(isNaN(allProducts[i].votes/allProducts[i].views)){
+      addElement('td', 0, trEl);
+    }else{
+      addElement('td', allProducts[i].votes/allProducts[i].views, trEl);
+    }
+  }
+}
+
+//Add element function
+function addElement(childElType, childText, ParentEl){
+  var childEl = document.createElement(childElType);
+  childEl.textContent = childText;
+  ParentEl.appendChild(childEl);
+}
 
 function render(){
-  //The index will increment 3 at a time 1,2,3 then 4,5,6
+  //The index will increment 3 at a time 1,2,3 then 4,5,6 then 7,8,9
   //it will be an index for a random unique number from the randomArray
   //This will return 3 random unique products for iteration
   imageOneEl.src = allProducts[randomArray[randomIndex]].filepath;
@@ -88,15 +138,20 @@ function render(){
 function handleClick(){
   var chosenImg = event.target.title;
 
+  // looks for the chosen img and adds a vote
   for(var i = 0; i < allProducts.length; i++){
     if(allProducts[i].name === chosenImg){
-      allProducts[i].views++;
+      allProducts[i].votes++;
     }
   }
 
   votes++;
+
+  //if we have 25 votes then turn off the event listener and make a table
   if(votes >= 25){
     productContainerEl.removeEventListener('click', handleClick);
+    makeHeader();
+    makeBody();
   }
   render();
 }
